11649c3f833400ec201b806bb0fdbfc1cc81cc56
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-01 14:51:01 +0000

    Update mobile menu toggle to link to authentication page

--- src/components/common/navbar/index.vue
+++ src/components/common/navbar/index.vue
@@ -19,22 +19,24 @@
       </div>
 
       <!-- Mobile Menu Toggle -->
-      <button v-else @click="toggleMenu" class="absolute right-8 lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
-      </button>
+      <NuxtLink v-else to="/auth">
+        <button @click="toggleMenu" class="absolute right-8 lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </NuxtLink>
 
       <!-- Navbar Links -->
       <div class="hidden lg:flex space-x-6 text-gray-700">
