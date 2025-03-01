be068519c9526daf7497659b50c1e3dfa3aae7aa
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-01 05:08:11 +0000

    style: enhance hero and app-wrapper for improved dark mode support

--- src/components/landing/hero/index.vue
+++ src/components/landing/hero/index.vue
@@ -9,11 +9,11 @@
     <div
       class="absolute inset-x-0 top-4 md:top-[12rem] lg:top-[20%] px-4 text-center max-w-screen-lg mx-auto"
     >
-      <h1 class="text-3xl md:text-3xl font-bold text-black">
+      <h1 class="text-3xl md:text-3xl font-bold text-black dark:text-white">
         Your complete platform for the web.
       </h1>
       <p
-        class="mt-2 text-sm sm:text-base md:text-lg text-gray-700 max-w-2xl mx-auto"
+        class="mt-2 text-sm sm:text-base md:text-lg text-gray-700 dark:text-gray-300 max-w-2xl mx-auto"
       >
         Vercel provides the developer tools and cloud infrastructure to build,
         scale, and secure a faster, more personalized web.
