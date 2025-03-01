bef5d51eb3b08d1ef81e5116b572e3a49e4e5769
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-01 15:08:58 +0000

    style: add hover effect to grid component for improved interactivity

--- src/components/landing/sections/grid.vue
+++ src/components/landing/sections/grid.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid md:grid-cols-2 border-t border-b dark:border-[#3b3b3b] gap-8"
+    class="grid md:grid-cols-2 border-t border-b dark:border-[#3b3b3b] gap-8 hover:scale-95 transition-all duration-300"
   >
     <slot />
   </div>
